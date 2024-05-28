7d4c589286265b07ae30f6486cee6e56a246e9b0
Mejillon <Mejillon@DESKTOP-LIBP5RV.cogra.com>
2024-05-28 10:16:14 -0500

    Modulo de Proveedores completo

--- src/co/edu/ucatolica/controlador/Controlador.java
+++ src/co/edu/ucatolica/controlador/Controlador.java
@@ -3,10 +3,13 @@ package co.edu.ucatolica.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import co.edu.ucatolica.modelo.Proveedor;
 import co.edu.ucatolica.vista.*;
 
 public class Controlador implements ActionListener {
     private VentanaPrincipal ventanaPrincipal;
+    private VistaProveedor visProv;
     
     public Controlador() {
         this.ventanaPrincipal = new VentanaPrincipal();
@@ -28,9 +31,15 @@ public class Controlador implements ActionListener {
             ventanaClientes.setVisible(true);
         } else if (e.getSource() == ventanaPrincipal.getBtnProveedores()) {
             // Abrir ventana de proveedores
-            VistaProveedor vistaProveedor = new VistaProveedor(null);
-            vistaProveedor.setVisible(true);
-        } else if (e.getSource() == ventanaPrincipal.getBtnProductos()) {
+        	  visProv = new VistaProveedor(this);
+              visProv.setVisible(true);
+              Proveedor prov = new Proveedor();
+              Proveedor[] ad=prov.verPro();
+              visProv.getpanelDatosProveedores().setProveedor(ad);
+
+          } else if (e.getSource() == ventanaPrincipal.getBtnProductos()) {
+        	
+        	
             // Abrir ventana de productos
             VentanaProductos ventanaProductos = new VentanaProductos();
             ventanaProductos.setVisible(true);
@@ -51,7 +60,68 @@ public class Controlador implements ActionListener {
             VentanaParametrizacion ventanaParametros = new VentanaParametrizacion();
             ventanaParametros.setVisible(true);
         }
+        
+        
+        //botones proveedor
+        if(e.getActionCommand().equals(visProv.getpanelAgregarProveedores().AGREGAR_PROVEEDOR)){
+            String nit=visProv.getpanelAgregarProveedores().getTxtNIT().getText();
+            String nombre=visProv.getpanelAgregarProveedores().getTxtNombre().getText();
+            String direccion =visProv.getpanelAgregarProveedores().getTxtDireccion().getText();
+            String telefono=visProv.getpanelAgregarProveedores().getTxtTelefono().getText();
+            String ciudad=visProv.getpanelAgregarProveedores().getTxtCiudad().getText();
+            
+            Proveedor prov = new Proveedor(nit, nombre, direccion, telefono, ciudad);
+            prov.guardarProveedor();
+            Proveedor[] ad=prov.verPro();
+            visProv.getpanelDatosProveedores().setProveedor(ad);
+            
+            
+        }
+        if(e.getActionCommand().equals(visProv.getpanelAgregarProveedores().MOFICAR_PROVEEDOR)){
+            String nit=visProv.getpanelAgregarProveedores().getTxtNIT().getText();
+            String nombre=visProv.getpanelAgregarProveedores().getTxtNombre().getText();
+            String direccion =visProv.getpanelAgregarProveedores().getTxtDireccion().getText();
+            String telefono=visProv.getpanelAgregarProveedores().getTxtTelefono().getText();
+            String ciudad=visProv.getpanelAgregarProveedores().getTxtCiudad().getText();
+            
+            String modifNit=visProv.getpanelFuncionesNit().getTxtNumero().getText(); 
+            Proveedor prov = new Proveedor(nit, nombre, direccion, telefono, ciudad);
+            prov.modifPro(modifNit, prov);
+            Proveedor[] ad=prov.verPro();
+            visProv.getpanelDatosProveedores().setProveedor(ad);
+        }
+        
+        if(e.getActionCommand().equals(visProv.getpanelFuncionesNit().BUSCAR_LEER)){
+            String modifNit=visProv.getpanelFuncionesNit().getTxtNumero().getText(); 
+            Proveedor prov = new Proveedor();
+            
+            visProv.mensageEmergente(prov.buscarProv(modifNit));
+            Proveedor[] ad=prov.verPro();
+            visProv.getpanelDatosProveedores().setProveedor(ad);
+        }
+        if(e.getActionCommand().equals(visProv.getpanelFuncionesNit().ELIMINAR)){
+            String modifNit=visProv.getpanelFuncionesNit().getTxtNumero().getText(); 
+            Proveedor prov = new Proveedor();
+            prov.eliminarProveedor(modifNit);
+            visProv.mensageEmergente("Se elimino Correctamente");
+            Proveedor[] ad=prov.verPro();
+            visProv.getpanelDatosProveedores().setProveedor(ad);
+        }
+
+        if(e.getActionCommand().equals(visProv.getpanelFuncionesNit().SALIR)){
+            visProv.dispose();
+            
+        }   
+        
     }
+
+	public VistaProveedor getVisProv() {
+		return visProv;
+	}
+
+	public void setVisProv(VistaProveedor visProv) {
+		this.visProv = visProv;
+	}
 
     
 }
