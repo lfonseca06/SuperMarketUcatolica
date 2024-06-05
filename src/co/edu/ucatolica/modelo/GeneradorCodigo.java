c0c677808b615bc42bfc147d149625526fa5fc57
JorgeEliecerValderramaR <167571941+JorgeEliecerValderramaR@users.noreply.github.com>
2024-06-05 15:55:05 -0500

    GeneraCodigo

--- src/co/edu/ucatolica/modelo/GeneradorCodigo.java
+++ src/co/edu/ucatolica/modelo/GeneradorCodigo.java
@@ -1,11 +1,12 @@
 package co.edu.ucatolica.modelo;
 
-
 import java.util.UUID;
 
 public class GeneradorCodigo {
     public static String generarCodigoCompra() {
         // Genera un código único para la compra utilizando UUID
-        return UUID.randomUUID().toString();
+        String codigo = UUID.randomUUID().toString().replace("-", "").toUpperCase();
+        // Toma los primeros 8 caracteres del UUID
+        return codigo.substring(0, 8);
     }
 }
